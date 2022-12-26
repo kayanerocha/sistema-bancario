6bf4584f3741a333df8f1ef3ea140f48b3c3f1e5
kayanerocha <kayannerch@gmail.com>
2022-12-26 14:27:29 -0300

    validando as opções do menu inicial

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+from banco import Banco
+from time import sleep
+
 class Menu:
     def __init__(self, titulo, opcoes):
         self.__titulo = titulo
@@ -9,6 +12,8 @@ class Menu:
         return self.__opcoes
     
     def imprimir_menu(self):
+        sleep(1)
+        print('-' * 30)
         print(f'{self.__titulo:^30} ')
         print('-' * 30)
         print('Escolha uma opção:')
@@ -18,11 +23,17 @@ class Menu:
     
     def solicitar_resposta(self):
         while True:
-            opcao = int(input('Opção: '))
-            if Menu.is_inteiro(opcao) and opcao >= 0 and opcao < len(self.opcoes):
-                self.__resposta = opcao
-                break
-        return self.__resposta
+            try:
+                opcao = int(input('Opção: '))
+                print('-' * 30)
+            except ValueError:
+                print('Informe uma opção válida.')
+                continue
+            else:
+                if opcao < 0 or opcao >= len(self.opcoes):
+                    print('Informe uma opção válida.')
+                else:
+                    return opcao
     
     @staticmethod
     def is_inteiro(numero):
